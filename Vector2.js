3a4f7ad2ba64f2bb5c75c8dcb44cd0a95f8301a7
TristanMS55 <13tjms@gmail.com>
2026-08-30 22:59:29 -0500

    Create Vector2.js

---
+++ Vector2.js
@@ -0,0 +1,29 @@
+class Vector2{
+
+    /**
+     * @type{Number} the x component of the vector
+     */
+
+    x
+
+    /**
+     * @type{Number} the y component of the vector
+     */
+
+    y
+
+    /**
+     * 
+     * @param {Number} x The x value of the new vector
+     * @param {Number} y The y value of the new vector
+     * 
+     * a constructor is similar to __init__ in Python
+     */
+
+    constructor(x, y){
+
+        //Assign incoming values to new instance
+        this.x = x
+        this.y = y
+    }
+}
